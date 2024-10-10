65035a5e060b5691a6d17294db99fe241f15b9a3
m6tm <maboadaniel.55@gmail.com>
2024-10-10 12:42:58 +0100

    Tailwindcss ajouté

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+    root: './public',
+    build: {
+        outDir: './dist',
+        rollupOptions: {
+            input: [
+                './resources/js/index.ts',
+                './resources/js/winwheel.js',
+                './resources/css/winwheel.css',
+            ],
+            output: {
+                entryFileNames: 'js/[name].js',
+                chunkFileNames: 'js/[name].js',
+                assetFileNames: 'assets/[name].[ext]',
+            }
+        }
+    },
+    plugins: [react()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler'
+        }
+      }
+    }
+})
